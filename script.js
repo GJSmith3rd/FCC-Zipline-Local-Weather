61e0c645e030ac330b034e46123600e538c56b0b
Gilbert Smith <gjsmith3rd@gmail.com>
2016-02-20 21:51:07 -0600

    Adding temp radio buttons, changing background.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ console.log('Document Load');
 
 $(document).ready(function () {
 
+    var response;
+
     console.log('***Get Data Before Geo');
 
     var url = 'http://api.openweathermap.org/data/2.5/weather?';
@@ -56,30 +58,71 @@ $(document).ready(function () {
 
         $.get(url, function (res) {
 
-            $('#weatherLocation').text(prep(res.name) +
-            ',' + prep(res.sys.country) +
-                ' Weather');
+            response = res;
 
-            $('#weatherIcon').attr('src', 'http://openweathermap.org/img/w/' +
-                prep(res.weather[0].icon) + '.png');
+            $('#weatherLocation').text(prep(res.name) +
+                ',' + prep(res.sys.country) +
+                ' Weather');
 
             $('#weatherCurrent').text((
                 prep(res.weather[0].main) +
                 ' ' +
-                prep(res.main.temp) +
-                ' F'));
+                roundFahrenheit()));
+
+            $('#weatherIcon').attr('src', 'http://openweathermap.org/img/w/' +
+                prep(res.weather[0].icon) + '.png');
 
             $('#weatherDesc').text(prep(res.weather[0].description));
 
-            $('#weatherDataRaw').text(JSON.stringify(res));
+            $('#weatherDataRaw1').text(JSON.stringify(res));
+            $('#weatherDataRaw2').text(JSON.stringify(response));
 
         });
 
-        function prep(data) {
+    }
 
-            return JSON.stringify(data).replace(/["]/g, '');
+    /*** RADIO BUTTON TOGGLE F/C ***/
+    $('#radioButtons').on('change', function () {
+
+        if ($('input[name=tempButton]:checked', '#myForm').val() === 'Fahrenheit') {
+
+            $('#weatherCurrent').text(
+                prep(response.weather[0].main) +
+                ' ' +
+                roundFahrenheit());
+
+        } else if ($('input[name=tempButton]:checked', '#myForm').val() === 'Celcius') {
+
+            $('#weatherCurrent').text((response.weather[0].main) +
+                ' ' +
+                convertCelcius());
 
         }
+
+    });
+
+    /*** ROUND FAHRENHEIT ***/
+    function roundFahrenheit() {
+
+        var tempF = Math.round(response.main.temp * 10) / 10;
+
+        return tempF;
+
+    }
+
+    /*** CONVERT FAHRENHEIT TO CELCIUS AND ROUND ***/
+    function convertCelcius() {
+
+        var tempC = (parseFloat(response.main.temp) - 32) * (5 / 9);
+        tempC = Math.round(tempC * 10) / 10;
+
+        return tempC;
+    }
+
+    /*** REMOVE CHARS FROM DATA ***/
+    function prep(data) {
+
+        return JSON.stringify(data).replace(/["]/g, '');
 
     }
 
